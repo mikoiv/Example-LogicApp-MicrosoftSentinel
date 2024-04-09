7b31f38d7415d64554dad6a09b4b81cbbd31cd61
Mikko Koivunen <61493860+mikoiv@users.noreply.github.com>
2024-04-09 16:51:42 +0300

    Update 1_createTable.ps1

--- 1_createTable.ps1
+++ 1_createTable.ps1
@@ -1,8 +1,8 @@
 # Variables for Azure Subscription ID, Log Analytics Resource Group and Log Analytics Workspace name.
-$TenantId = "4a5d1d90-5ce3-4b97-9b81-028dee0c72dc"
-$SubscriptionId = "d0f4f21b-3cee-4c8e-8514-04f2c3786b4b"
-$LogAnalyticsRG = "rg-logs-sec-001"
-$LogAnalyticsWorkspace = "law-secopslab-sec-001"
+$TenantId = ""
+$SubscriptionId = ""
+$LogAnalyticsRG = ""
+$LogAnalyticsWorkspace = ""
 
 # Connect to Azure
 Connect-AzAccount -Tenant $TenantId
